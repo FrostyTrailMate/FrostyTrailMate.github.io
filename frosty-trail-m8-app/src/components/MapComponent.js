7908ea8c2878c73f56e435fd45d65ecc99e64234
Sebastian Suarez Andrade <157390893+SebastianGeotech@users.noreply.github.com>
2024-02-07 14:23:39 +0000

    GeoJson update! working

--- frosty-trail-m8-app/src/components/MapComponent.js
+++ frosty-trail-m8-app/src/components/MapComponent.js
@@ -1,22 +1,30 @@
-// MyMapComponent.js
-
-import React from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import React, { useEffect, useState } from 'react';
+import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import YosemiteBoundaries from './geojsons/YosemiteBoundaries.json'; // Import GeoJSON data file
 
 const MapComponent = () => {
+  const [geojsonData, setGeojsonData] = useState(null);
+
+  useEffect(() => {
+    setGeojsonData(YosemiteBoundaries); // Set GeoJSON data when component mounts
+  }, []);
+
   return (
-    <div style={{ backgroundColor: '#242424', padding: '60px' }}> {/* Set the background color to black */}
+    <div style={{ backgroundColor: '#242424', padding: '60px' }}>
       <MapContainer
-        center={([37.8651, -119.5383])} // Set the initial center (latitude, longitude) (Lisbon)
-        zoom={10} // Set the initial zoom level
-        style={{ height: '550px', width: '85%', margin: '0 auto' }} // Set the map container size and center it
+        center={[37.8451, -119.5383]}
+        zoom={10}
+        style={{ height: '690px', width: '85%', margin: '0 auto' }}
       >
         <TileLayer
           url="https://tile.thunderforest.com/outdoors/{z}/{x}/{y}.png?apikey=749cd9dc6622478d9454b931ded7943d"
           subdomains={['mt0', 'mt1', 'mt2', 'mt3']}
           attribution='© Thunderforest by Gravitystorm Limited.'
         />
+        {geojsonData && (
+          <GeoJSON data={geojsonData} />
+        )}
       </MapContainer>
     </div>
   );
--- frosty-trail-m8-app/src/components/pages/About.js
+++ frosty-trail-m8-app/src/components/pages/About.js
@@ -5,10 +5,10 @@ function About() {
   return (
     <>
       <div className='About'>
-        <p>Welcome to Frosty Trail Mate, your essential companion for navigating Yosemite National Park!
+        <p><strong>Welcome to Frosty Trail Mate, your essential companion for navigating Yosemite National Park!
           Our project emerged from the desire to simplify the hiking experience by providing precise information 
           about the location of snow on the trails. Explore Yosemite confidently with our user-friendly platform, 
-          ensuring you stay informed about snow locations for a seamless and enjoyable hiking adventure.
+          ensuring you stay informed about snow locations for a seamless and enjoyable hiking adventure!</strong>
         </p>
       </div>
     </>
