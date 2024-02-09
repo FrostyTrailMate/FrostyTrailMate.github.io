fdbc0d12a3e24cf18ee77cf7129b9fbd75c26c15
Sebastian Suarez Andrade <157390893+SebastianGeotech@users.noreply.github.com>
2024-02-09 15:34:00 +0000

    Last table working with date field updated

--- SQL Queries/CreateTables.sql
+++ SQL Queries/CreateTables.sql
@@ -17,17 +17,18 @@ CREATE TABLE results (
 /*CREATE TABLE resultstestapi (
     id_res SERIAL PRIMARY KEY,
 	altitude INT,
-    snowcover DECIMAL(10, 1) ,
-	darea VARCHAR(30));
+    snowcover DECIMAL(10, 1),
+	darea VARCHAR(30);
+	rdate DATE);
 	
-INSERT INTO resultstestapi (id_res, altitude, snowcover, darea) VALUES
-	(1, 100, 0.5, 'Yosemite National Park'),
-	(2, 200, 10.5, 'Yosemite National Park'),
-	(3, 300, 20.5, 'Yosemite National Park'),
-	(4, 400, 30.5, 'Yosemite National Park'),
-	(5, 500, 40.5, 'Yosemite National Park'),
-	(6, 600, 50.5, 'Yosemite National Park'),
-	(7, 700, 60.5, 'Yosemite National Park'),
-	(8, 800, 70.5, 'Yosemite National Park'),
-	(9, 900, 80.5, 'Yosemite National Park'),
-	(10, 1000, 90.5, 'Yosemite National Park');*/
+INSERT INTO resultstestapi (id_res, altitude, snowcover, darea, rdate) VALUES
+	(1, 100, 0.5, 'Yosemite National Park', '2021-01-01'),
+	(2, 200, 10.5, 'Yosemite National Park', '2021-01-01'),
+	(3, 300, 20.5, 'Yosemite National Park', '2021-01-01'),
+	(4, 400, 30.5, 'Yosemite National Park', '2021-01-01'),
+	(5, 500, 40.5, 'Yosemite National Park', '2021-01-01'),
+	(6, 600, 50.5, 'Yosemite National Park', '2021-01-01'),
+	(7, 700, 60.5, 'Yosemite National Park', '2021-01-01'),
+	(8, 800, 70.5, 'Yosemite National Park', '2021-01-01'),
+	(9, 900, 80.5, 'Yosemite National Park', '2021-01-01'),
+	(10, 1000, 90.5, 'Yosemite National Park','2021-01-01');*/
