e1ec8fa7c7875a5b57abebcf5d3dd7ab4a6c62ee
Sebastian Suarez Andrade <157390893+SebastianGeotech@users.noreply.github.com>
2024-02-25 16:56:44 +0000

    New basempas and toggles for the map

--- frosty-trail-m8-app/src/components/MapComponent.js
+++ frosty-trail-m8-app/src/components/MapComponent.js
@@ -2,13 +2,29 @@ import React, { useState } from 'react';
 import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import './CCStyles/MapComponent.css'; // Import external CSS file
-import TrailsYosemite from './geojsons/trails.json'; // Import GeoJSON data file
-import ElevationPolygons from './geojsons/ElevationPolygons.json'; // Import GeoJSON data file for ElevationPolygons
-
+import TrailsYosemite from './geojsons/Trails.json'; // Import GeoJSON data file
+import BoundaryPolygon from './geojsons/YosemiteBoundary.json'; // Import GeoJSON data file
 
 const MapComponent = () => {
+  const [basemap, setBasemap] = useState('thunderforest');
   const [showTrails, setShowTrails] = useState(true);
   const [showElevation, setShowElevation] = useState(true);
+
+  const basemapUrls = {
+    thunderforest: 'https://tile.thunderforest.com/outdoors/{z}/{x}/{y}.png?apikey=749cd9dc6622478d9454b931ded7943d',
+    openStreetMap: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    stamenTerrain: 'https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png',
+  };
+
+  const basemapAttributions = {
+    thunderforest: '© Thunderforest by Gravitystorm Limited.',
+    openStreetMap: '© OpenStreetMap contributors',
+    stamenTerrain: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.',
+  };
+
+  const handleBasemapChange = (newBasemap) => {
+    setBasemap(newBasemap);
+  };
 
   const handleTrailsToggle = () => {
     setShowTrails(!showTrails);
@@ -24,10 +40,9 @@ const MapComponent = () => {
   };
 
   const lightBluePolygonStyle = {
-    fillColor: '#add8e6', // Light blue color
     color: '#555', // Grey color for contour lines
-    weight: 0.5, // Adjust the weight of the polygon
-    fillOpacity: 0.5,
+    weight: 2, // Adjust the weight of the polygon
+    fill: false, // Fill the polygon with color
   };
 
   const onEachFeature = (feature, layer) => {
@@ -39,36 +54,54 @@ const MapComponent = () => {
   return (
     <div className="map-container">
       <div className="toggle-container">
-        <label className="toggle-label">
-          <input
-            type="checkbox"
-            checked={showTrails}
-            onChange={handleTrailsToggle}
-          />
-          <span className="toggle-text">Hiking Trails</span>
-        </label>
-        <label className="toggle-label">
-          <input
-            type="checkbox"
-            checked={showElevation}
-            onChange={handleElevationToggle}
-          />
-          <span className="toggle-text">Snow Coverage</span>
-        </label>
+        <div className="basemap-toggles">
+          {Object.keys(basemapUrls).map((key) => (
+            <label key={key} className="basemap-toggle">
+              <input
+                type="radio"
+                name="basemap"
+                value={key}
+                checked={basemap === key}
+                onChange={() => handleBasemapChange(key)}
+              />
+              {key === 'thunderforest' && 'Thunderforest'}
+              {key === 'openStreetMap' && 'OpenStreetMap'}
+              {key === 'stamenTerrain' && 'Stamen Terrain'}
+            </label>
+          ))}
+        </div>
+        <div className="geojson-toggles">
+          <label className="geojson-toggle">
+            <input
+              type="checkbox"
+              checked={showTrails}
+              onChange={handleTrailsToggle}
+            />
+            <span className="toggle-text">Hiking Trails</span>
+          </label>
+          <label className="geojson-toggle">
+            <input
+              type="checkbox"
+              checked={showElevation}
+              onChange={handleElevationToggle}
+            />
+            <span className="toggle-text">Boundary</span>
+          </label>
+        </div>
       </div>
       <MapContainer
         center={[37.8451, -119.5383]}
-        zoom={11}
+        zoom={10}
         className="leaflet-container"
       >
         <TileLayer
-          url="https://tile.thunderforest.com/outdoors/{z}/{x}/{y}.png?apikey=749cd9dc6622478d9454b931ded7943d"
+          url={basemapUrls[basemap]}
           subdomains={['mt0', 'mt1', 'mt2', 'mt3']}
-          attribution="© Thunderforest by Gravitystorm Limited."
+          attribution={basemapAttributions[basemap]}
         />
 
         {showElevation && (
-          <GeoJSON data={ElevationPolygons} style={lightBluePolygonStyle} onEachFeature={onEachFeature} />
+          <GeoJSON data={BoundaryPolygon} style={lightBluePolygonStyle} onEachFeature={onEachFeature} />
         )}
 
         {showTrails && (
@@ -80,8 +113,5 @@ const MapComponent = () => {
 };
 
 export default MapComponent;
-
-
-
 
 
