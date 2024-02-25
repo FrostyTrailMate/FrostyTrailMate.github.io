0c26610314a4ca60a5b1a9a59c299c9ac4cc77ac
chubacha <154326983+chubacha@users.noreply.github.com>
2024-02-25 23:57:19 +0000

    add 'Create' link

--- frosty-trail-m8-app/src/App.js
+++ frosty-trail-m8-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
+import Create from './components/pages/Create';
 import About from './components/pages/About';
 import Footer from './components/Footer';
 import Create from './components/pages/Create';
@@ -16,7 +17,8 @@ function App() {
       <Router>
       <Navbar />
         <Routes>
-          <Route exact path='/Home' element={<Home />} />
+          <Route exact path='/' element={<Home />} />
+          <Route path='/Create' element={<Create />} />
           <Route path='/About' element={<About />} />
           <Route path='/Create' element={<Create />} />
         </Routes>
