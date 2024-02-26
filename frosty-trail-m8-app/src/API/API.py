24a974bd7e8e51ff162616235d1b8f266f670163
Sebastian Suarez Andrade <157390893+SebastianGeotech@users.noreply.github.com>
2024-02-26 00:29:30 +0000

    removal of geoalchemy2

--- frosty-trail-m8-app/src/API/API.py
+++ frosty-trail-m8-app/src/API/API.py
@@ -2,7 +2,6 @@ from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import subprocess
-from geoalchemy2 import Geometry
 
 DB_CONFIG = {
     "database": "FTM8",
