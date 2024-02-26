2ca0dcbaf7ae570241becb55b290948bc553cd77
chubacha <154326983+chubacha@users.noreply.github.com>
2024-02-26 00:05:12 +0000

    Ctrl-Z

--- frosty-trail-m8-app/src/components/pages/Create.js
+++ frosty-trail-m8-app/src/components/pages/Create.js
@@ -1,11 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import '../CCStyles/Create.css'; // Assuming your styles are here
+import '../CCStyles/Create.css';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import axios from 'axios';
-import { MapContainer, TileLayer, Rectangle, useMapEvents } from 'react-leaflet';
-import 'leaflet-draw';
-import 'leaflet-draw/dist/leaflet.draw.css';
 
 
 function Create() {
@@ -17,29 +14,6 @@ function Create() {
   const [coordinates, setCoordinates] = useState(null);
   const [apiStatus, setApiStatus] = useState({});
 
-  // State to store rectangle bounds
-  const [rectangleBounds, setRectangleBounds] = useState(null);
-
-  // Leaflet 'useMapEvents' hook to interact with map
-  function AddRectangleToMap() {
-    const map = useMapEvents({
-      click() { 
-        // Reset rectangle on each new click
-        setRectangleBounds(null); 
-      },
-      draw: (event) => {
-        // On 'drawcreated' event, get rectangle details
-        if (event.layerType === 'rectangle') {
-          setRectangleBounds(event.layer.getBounds());
-        }
-      },
-    });
-
-    return rectangleBounds ? (
-      <Rectangle bounds={rectangleBounds} pathOptions={{ color: 'blue' }} />
-    ) : null;
-  }
-
   const sendDataToAPI = () => {
     const data = {
       startDate: startDate,
@@ -48,14 +22,6 @@ function Create() {
       distance: distance,
       rasterBand: rasterBand,
     };
-
-    // Include coordinates from rectangleBounds
-    if (rectangleBounds) {
-      data.xmin = rectangleBounds.getSouthWest().lng;
-      data.ymin = rectangleBounds.getSouthWest().lat;
-      data.xmax = rectangleBounds.getNorthEast().lng;
-      data.ymax = rectangleBounds.getNorthEast().lat;
-    }
 
     axios.post('http://127.0.0.1:5000/api/create', data)
       .then(response => {
@@ -98,38 +64,38 @@ function Create() {
           placeholderText='Select End Date'
           className='inputField'
         />
-      </div>
-      <div>
+        </div>
+        <div>
         <label htmlFor='area_name'>Area Name: </label>
         <input type='text' id='area_name' value={areaName} onChange={e => setAreaName(e.target.value)} className='inputField' />
-      </div>
-      <div>
-        <label htmlFor='distance'>Distance between sampling points (.005 = 500 meters): </label>
+        </div>
+        <div>
+        <label htmlFor='distance'>Distance between sampling (.005 = 500 meters): </label>
         <input type='text' id='distance' value={distance} onChange={e => setDistance(e.target.value)} className='inputFieldDist' />
-      </div>
-      <div>
+        <div>
         <label>Choose Raster Band:</label>
+        </div>
+        <div className='radioGroup'>
+          <input type='radio' id='vv' name='raster_band' value='VV' checked={rasterBand === 'VV'} onChange={() => setRasterBand('VV')} />
+          <label htmlFor='vv'>VV</label>
+          <input type='radio' id='vh' name='raster_band' value='VH' checked={rasterBand === 'VH'} onChange={() => setRasterBand('VH')} />
+          <label htmlFor='vh'>VH</label>
+        </div>
+        <button onClick={sendDataToAPI} className='submitButton'>Send Data to API</button>
       </div>
-      <div className='radioGroup'>
-        <input type='radio' id='vv' name='raster_band' value='VV' checked={rasterBand === 'VV'} onChange={() => setRasterBand('VV')} />
-        <label htmlFor='vv'>VV</label>
-        <input type='radio' id='vh' name='raster_band' value='VH' checked={rasterBand === 'VH'} onChange={() => setRasterBand('VH')} />
-        <label htmlFor='vh'>VH</label> 
-      </div>
-
-      {/* Leaflet Map Section */}
-      <div className='leaflet-container'>
-        <MapContainer center={[0, 0]} zoom={2}> 
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <AddRectangleToMap /> 
-        </MapContainer>
-      </div>
-
-      <button onClick={sendDataToAPI} className='submitButton'>Send Data to API</button>
-      {/* ... (the rest of your component: coordinates display, apiStatus message) */}
+      {coordinates && (
+        <div className='coordinates'>
+          <p>xmin: {coordinates.xmin}</p>
+          <p>ymin: {coordinates.ymin}</p>
+          <p>xmax: {coordinates.xmax}</p>
+          <p>ymax: {coordinates.ymax}</p>
+        </div>
+      )}
+      {apiStatus.message && (
+        <div className={`apiMessage ${apiStatus.success ? 'success' : 'error'}`}>
+          {apiStatus.message}
+        </div>
+      )}
     </>
   );
 }
