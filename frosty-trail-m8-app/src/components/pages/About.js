985918ae2b89d91ae3fda902151ec17897e46fcb
Sebastian Suarez Andrade <157390893+SebastianGeotech@users.noreply.github.com>
2024-02-28 10:56:47 +0000

    Edits

--- frosty-trail-m8-app/src/components/pages/About.js
+++ frosty-trail-m8-app/src/components/pages/About.js
@@ -6,7 +6,7 @@ function About() {
   return (
   <>
     <div className='textabout'>
-      <span>Welcome to Frosty Trail Mate, your essential companion for navigating hiking trails, anywhere
+      <span><strong>Welcome to Frosty Trail Mate</strong>, your essential companion for navigating hiking trails, anywhere
       in the world! Our project emerged from the desire to simplify the hiking experience by providing precise 
       information about the location of snow on the trails. Explore trails confidently with our user-friendly 
       platform, ensuring you stay informed about snow locations for a seamless and enjoyable hiking adventure!
@@ -16,7 +16,7 @@ function About() {
         <source src={video} type="video/mp4" />
       </video>
       <div className='textabout2'>
-      <span >By: Christopher H, Carolina C & Sebastian S</span>
+      <span ><strong>By:</strong> Christopher H, Sebastian S and Carolina</span>
       </div>
     </>
   );
